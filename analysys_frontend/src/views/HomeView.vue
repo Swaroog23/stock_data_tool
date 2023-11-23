9b412b50896ad3f6b69f5bd5238daf1d827f9ed5
Dominik Popławski <poplawd@gmail.com>
2023-11-23 13:15:46 +0100

    frontend module structure

---
+++ analysys_frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                message: "test 1",
+            }
+        }
+    }
+</script>
+
+<style>
+    .home {
+        text-align: center;
+        margin-top: 50px;
+    }
+</style>
